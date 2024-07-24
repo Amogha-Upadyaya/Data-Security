84fd783ac0a7cfb67925016bbcd4f97ef9fcddb4
Amogha Upadyaya <120311753+Amogha-Upadyaya@users.noreply.github.com>
2024-07-24 14:01:32 +0000

    Added Caesar Cipher Code v1

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,1 +1,20 @@
+# This program aims to encrypt the inputted word/statement via Caesar Cipher
+# Program:
 
+# 1. Ask user to input the word/statement
+text = input("Enter text to encrypt: ")
+# 2. Ask user to input the shift value
+shift = int(input("Enter shift value: "))
+# 3. Encrypt the word/statement via Caesar Cipher
+encrypted_text = ""
+for char in text:
+    if char.isalpha():
+        if char.isupper():
+            encrypted_text += chr((ord(char) + shift - 65) % 26)
+        else:
+            encrypted_text += chr((ord(char) + shift - 97) % 26)
+    else:
+        encrypted_text += char
+
+# 4. Display the encrypted word/statement
+print("Encrypted text: ", encrypted_text)
