038133e8957c03545f747ceb658c1b0ca48e3d8b
Amogha-Upadyaya <amoghaupadyaya.official@gmail.com>
2024-08-05 20:50:02 +0530

    Changed rsa to RSA_Algorithm

--- Code_Files/RSA_Algorithm/main.py
+++ Code_Files/RSA_Algorithm/main.py
@@ -1,11 +1,11 @@
-import rsa
+import RSA_Algorithm
 
 def main():
     p = int(input("Enter a prime number p: "))
     q = int(input("Enter another prime number q: "))
     
     try:
-        pub_key, priv_key = rsa.generate_rsa_keys(p, q)
+        pub_key, priv_key = RSA_Algorithm.generate_rsa_keys(p, q)
     except ValueError as e:
         print(e)
         return
@@ -14,10 +14,10 @@ def main():
     print("Private Key: ", priv_key)
     
     message = input("Enter a message to encrypt: ")
-    encrypted_message = rsa.encrypt(message, pub_key)
+    encrypted_message = RSA_Algorithm.encrypt(message, pub_key)
     print("Encrypted message:", encrypted_message)
     
-    decrypted_message = rsa.decrypt(encrypted_message, priv_key)
+    decrypted_message = RSA_Algorithm.decrypt(encrypted_message, priv_key)
     print("Decrypted message:", decrypted_message)
 
 if __name__ == "__main__":
